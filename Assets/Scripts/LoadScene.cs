69063d83f8091f2652c9f33ce9f3ba1c3ee5c4c8
Matthewit Dechatech <mdechatech@gmail.com>
2016-02-21 14:08:49 -0600

    Quick intro scene

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts
+{
+    public class LoadScene : MonoBehaviour
+    {
+        public void Load(int index)
+        {
+            SceneManager.LoadScene(index);
+        }
+
+        public void Load(string name)
+        {
+            SceneManager.LoadScene(name);
+        }
+    }
+}
