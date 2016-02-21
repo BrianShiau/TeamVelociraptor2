89ca06d1fa37171bfd3d32223a30b553b2d491bb
Raymond Yang <ray.yang@utexas.edu>
2016-02-20 22:26:11 -0600

    Player now respawns with a delay

--- Assets/Scripts/PlayerRespawn.cs
+++ Assets/Scripts/PlayerRespawn.cs
@@ -5,18 +5,33 @@ public class PlayerRespawn : MonoBehaviour {
 
     public GameObject person;
     public GameObject player;
+    public bool respawning = false;
 
     // Use this for initialization
     void Start () {
-
+        Instantiate(person, transform.position, Quaternion.identity);
     }
 	
 	// Update is called once per frame
 	void Update () {
         player = GameObject.Find("player(Clone)");
+
+        if(player == null && respawning == false)
+        {
+            StartCoroutine(Respawn());
+        }
+	}
+
+    //Respawns the player with a delay
+    IEnumerator Respawn()
+    {
+        respawning = true;
+        yield return new WaitForSeconds(5);
+
         if (player == null && person != null)
         {
             Instantiate(person, transform.position, Quaternion.identity);
         }
-	}
+        respawning = false;
+    }
 }
